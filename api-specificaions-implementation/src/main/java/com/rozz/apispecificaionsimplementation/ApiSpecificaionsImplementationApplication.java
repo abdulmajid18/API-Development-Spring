4adec885bb17b41e21c323a05d5b87479a6783b7
abdulmajid18 <nuhuabdulmajid58@gmail.com>
2023-02-21 01:09:35 +0000

    spring initializer

---
+++ api-specificaions-implementation/src/main/java/com/rozz/apispecificaionsimplementation/ApiSpecificaionsImplementationApplication.java
@@ -0,0 +1,13 @@
+package com.rozz.apispecificaionsimplementation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiSpecificaionsImplementationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiSpecificaionsImplementationApplication.class, args);
+	}
+
+}
---
+++ api-specificaions-implementation/src/test/java/com/rozz/apispecificaionsimplementation/ApiSpecificaionsImplementationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rozz.apispecificaionsimplementation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiSpecificaionsImplementationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
