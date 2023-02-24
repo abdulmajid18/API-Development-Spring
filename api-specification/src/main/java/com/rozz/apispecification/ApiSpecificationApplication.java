ce8d8b62cb58f7db04c3665eb07fd28662789847
abdulmajid18 <nuhuabdulmajid58@gmail.com>
2023-02-24 14:10:34 +0000

    new initializer

---
+++ api-specification/src/main/java/com/rozz/apispecification/ApiSpecificationApplication.java
@@ -0,0 +1,13 @@
+package com.rozz.apispecification;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiSpecificationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiSpecificationApplication.class, args);
+	}
+
+}
---
+++ api-specification/src/test/java/com/rozz/apispecification/ApiSpecificationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rozz.apispecification;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiSpecificationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
