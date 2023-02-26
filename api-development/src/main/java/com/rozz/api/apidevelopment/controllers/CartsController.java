20045659d9dd0280cb5a5dceadb0bf5f0a6f6cc7
abdulmajid18 <nuhuabdulmajid58@gmail.com>
2023-02-26 14:45:29 +0000

    added pathmapping and paramTypes

--- api-development/src/main/java/com/rozz/api/apidevelopment/controllers/CartsController.java
+++ api-development/src/main/java/com/rozz/api/apidevelopment/controllers/CartsController.java
@@ -1,12 +1,19 @@
 package com.rozz.api.apidevelopment.controllers;
 
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
 import org.slf4j.Logger;
 
 import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import static org.springframework.http.ResponseEntity.ok;
@@ -20,12 +27,16 @@ public class CartsController implements CartApi {
     private static final Logger log = LoggerFactory.getLogger(CartsController.class);
 
     @Override
-    public ResponseEntity<List<Item>> addCartItemsByCustomerId(String customerId, Item item) {
+    @PostMapping(value = "/api/v1/carts/{customerId}/items")
+    public ResponseEntity<List<Item>> addCartItemsByCustomerId(@PathVariable("customerId") String customerId,
+            @RequestBody Item item) {
         log.info("Request for customer ID: {}\nItem: {}", customerId, item);
         return ok(Collections.EMPTY_LIST);
+
     }
 
-    public ResponseEntity<List<Cart>> getCartByCustomerId(String customerId) {
+    @GetMapping(value = "/api/v1/carts/{customerId}/items")
+    public ResponseEntity<List<Cart>> getCartByCustomerId(@PathVariable("customerId") String customerId) {
         throw new RuntimeException("Manual Exception thrown");
     }
 }
