9071dcc90c247004a773ef210fa81c2fedcdec16
abdulmajid18 <nuhuabdulmajid58@gmail.com>
2023-02-28 18:46:00 +0000

    Cart Entity

--- api-development/src/main/java/com/rozz/api/apidevelopment/entity/CartEntity.java
+++ api-development/src/main/java/com/rozz/api/apidevelopment/entity/CartEntity.java
@@ -1,5 +1,25 @@
 package com.rozz.api.apidevelopment.entity;
 
+import java.util.UUID;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToOne;
+import jakarta.persistence.Table;
+
+@Entity
+@Table
 public class CartEntity {
 
+    @Id
+    @GeneratedValue
+    @Column(name = "ID", updatable = false, nullable = false)
+    private UUID id;
+
+    @OneToOne
+    @JoinColumn(name = "USER_ID", referencedColumnName = "ID")
+    private UserEntity user;
 }
