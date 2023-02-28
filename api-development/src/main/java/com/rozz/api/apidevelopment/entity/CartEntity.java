973b338c83a54680d3231d5a8ea53529ca4c950e
abdulmajid18 <nuhuabdulmajid58@gmail.com>
2023-02-28 20:46:59 +0000

    update

--- api-development/src/main/java/com/rozz/api/apidevelopment/entity/CartEntity.java
+++ api-development/src/main/java/com/rozz/api/apidevelopment/entity/CartEntity.java
@@ -1,12 +1,16 @@
 package com.rozz.api.apidevelopment.entity;
 
 import java.util.UUID;
+import java.util.List;
+import java.util.Objects;
+import java.util.Collections;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinTable;
 import jakarta.persistence.OneToOne;
 import jakarta.persistence.Table;
 
@@ -22,6 +26,9 @@ public class CartEntity {
     @OneToOne
     @JoinColumn(name = "USER_ID", referencedColumnName = "ID")
     private UserEntity user;
+
+    @JoinTable(name = "CART_ITEM", joinColumns = @JoinColumn(name = "CART_ID"), inverseJoinColumns = @JoinColumn(name = "ITEM_ID"))
+    private List<ItemEntity> items = Collections.emptyList();
 
     public UUID getId() {
         return id;
@@ -39,6 +46,32 @@ public class CartEntity {
     public CartEntity setUser(UserEntity user) {
         this.user = user;
         return this;
+    }
+
+    public List<ItemEntity> getItems() {
+        return items;
+    }
+
+    public CartEntity setItems(List<ItemEntity> items) {
+        this.items = items;
+        return this;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        CartEntity that = (CartEntity) o;
+        return user.equals(that.user) && Objects.equals(items, that.items);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(user, items);
     }
 
 }
