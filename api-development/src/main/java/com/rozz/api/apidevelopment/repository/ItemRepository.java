9cc4aca287e14a4fe78e9c14828eaa51cb5591c0
abdulmajid18 <nuhuabdulmajid58@gmail.com>
2023-03-02 16:23:03 +0000

    customized sql query

--- api-development/src/main/java/com/rozz/api/apidevelopment/repository/ItemRepository.java
+++ api-development/src/main/java/com/rozz/api/apidevelopment/repository/ItemRepository.java
@@ -1,11 +1,20 @@
 package com.rozz.api.apidevelopment.repository;
 
 import java.util.UUID;
+import java.util.List;
 
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
 import com.rozz.api.apidevelopment.entity.ItemEntity;
 
 public interface ItemRepository extends CrudRepository<ItemEntity, UUID> {
+    @Query(value = "select i.* from ecomm.cart c, ecomm.item i, ecomm.user u, ecomm.cart_item ci where u.id=:customerId and c.user_id=u.id and c.id=ci.cart_id and i.id=ci.item_id", nativeQuery = true)
+    Iterable<ItemEntity> findByCustomerId(String customerId);
+
+    @Modifying
+    @Query(value = "delete from ecomm.cart_item where item_id in (:ids) and cart_id = :cartId", nativeQuery = true)
+    void deleteCartItemJoinById(List<UUID> ids, UUID cartId);
 
 }
