f32c8c6ff542772b1d8d6fb29a47a0c9c2bf4d43
abdulmajid18 <nuhuabdulmajid58@gmail.com>
2023-03-02 16:28:15 +0000

    customized sql query

--- api-development/src/main/java/com/rozz/api/apidevelopment/repository/CartRepository.java
+++ api-development/src/main/java/com/rozz/api/apidevelopment/repository/CartRepository.java
@@ -1,11 +1,15 @@
 package com.rozz.api.apidevelopment.repository;
 
+import java.util.Optional;
 import java.util.UUID;
 
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 
 import com.rozz.api.apidevelopment.entity.CartEntity;
 
 public interface CartRepository extends CrudRepository<CartEntity, UUID> {
-
+    @Query("select c from CartEntity c join c.user u where u.id = :customerId")
+    public Optional<CartEntity> findByCustomerId(@Param("custoomerId") UUID customerId);
 }
