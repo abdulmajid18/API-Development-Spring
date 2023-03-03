d5c8087e59f378c12b002ca7bee062df5e22f399
abdulmajid18 <nuhuabdulmajid58@gmail.com>
2023-03-03 23:00:06 +0000

    updated

--- api-development/src/main/java/com/rozz/api/apidevelopment/repository/CartRepository.java
+++ api-development/src/main/java/com/rozz/api/apidevelopment/repository/CartRepository.java
@@ -10,6 +10,6 @@ import org.springframework.data.repository.query.Param;
 import com.rozz.api.apidevelopment.entity.CartEntity;
 
 public interface CartRepository extends CrudRepository<CartEntity, UUID> {
-    @Query("select c from CartEntity c join c.user u where u.id = :customerId")
-    public Optional<CartEntity> findByCustomerId(@Param("custoomerId") UUID customerId);
+    @Query("select c from CartEntity c join c.customer u where u.id = :customerId")
+    public Optional<CartEntity> findByCustomerId(@Param("customerId") UUID customerId);
 }
