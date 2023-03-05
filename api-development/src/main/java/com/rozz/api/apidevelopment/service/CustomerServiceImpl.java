2f03f9c046045e1249056c2d215ea7adffe6293d
abdulmajid18 <nuhuabdulmajid58@gmail.com>
2023-03-05 14:50:30 +0000

    Customer Service Impl

--- api-development/src/main/java/com/rozz/api/apidevelopment/service/CustomerServiceImpl.java
+++ api-development/src/main/java/com/rozz/api/apidevelopment/service/CustomerServiceImpl.java
@@ -2,15 +2,40 @@ package com.rozz.api.apidevelopment.service;
 
 import java.util.Optional;
 import java.util.UUID;
+import java.util.Collections;
+import java.util.List;
+import java.util.Objects;
+import static java.util.stream.Collectors.toList;
+import java.util.stream.StreamSupport;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Service;
+
+import com.rozz.api.apidevelopment.dto.CustomerRequest;
+import com.rozz.api.apidevelopment.dto.CustomerResponse;
 import com.rozz.api.apidevelopment.entity.CustomerEntity;
 import com.rozz.api.apidevelopment.repository.CustomerRepository;
 
+@Service
 public class CustomerServiceImpl implements CustomerService {
   private CustomerRepository repository;
 
+  private static final Logger log = LoggerFactory.getLogger(CustomerServiceImpl.class);
+
   public CustomerServiceImpl(CustomerRepository repository) {
     this.repository = repository;
+  }
+
+  @Override
+  public List<CustomerResponse> getAllCustomers() {
+    Iterable<CustomerEntity> customers = repository.findAll();
+    return toListModel(customers);
+  }
+
+  @Override
+  public Optional<CustomerResponse> getCustomerById(String id) {
+    return repository.findById(UUID.fromString(id)).map(this::mapToCustomer);
   }
 
   @Override
@@ -18,14 +43,35 @@ public class CustomerServiceImpl implements CustomerService {
     repository.deleteById(UUID.fromString(id));
   }
 
-  @Override
-  public Iterable<CustomerEntity> getAllCustomers() {
-    return repository.findAll();
+  private CustomerResponse mapToCustomer(CustomerEntity customerEntity) {
+    CustomerResponse customer = new CustomerResponse();
+    customer.setFirstName(customerEntity.getFirstName())
+        .setLastName(customerEntity.getLastName())
+        .setEmail(customerEntity.getEmail())
+        .setUsername(customerEntity.getUsername())
+        .setPhone(customerEntity.getPhone())
+        .setUserStatus(customerEntity.getUserStatus());
+    return customer;
   }
 
-  @Override
-  public Optional<CustomerEntity> getCustomerById(String id) {
-    return repository.findById(UUID.fromString(id));
+  public List<CustomerResponse> toListModel(Iterable<CustomerEntity> entities) {
+    if (Objects.isNull(entities)) {
+      return Collections.emptyList();
+    }
+    return StreamSupport.stream(entities.spliterator(), false).map(this::mapToCustomer).collect(toList());
+  }
+
+  public void createCustomer(CustomerRequest customerRequest) {
+    CustomerEntity customerEntity = new CustomerEntity();
+
+    customerEntity.setFirstName(customerRequest.getFirstName())
+        .setLastName(customerRequest.getLastName())
+        .setUsername(customerRequest.getUsername())
+        .setEmail(customerRequest.getEmail())
+        .setPassword(customerRequest.getPassword())
+        .setPhone(customerRequest.getPhone());
+    repository.save(customerEntity);
+    log.info("Request for Creating customer Name: {}\n", customerEntity.getUsername());
   }
 
 }
