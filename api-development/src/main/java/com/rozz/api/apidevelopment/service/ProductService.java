29ea457e8cc59caecec93a61321d6b8f826a243f
abdulmajid18 <nuhuabdulmajid58@gmail.com>
2023-03-05 15:37:18 +0000

    add product method

---
+++ api-development/src/main/java/com/rozz/api/apidevelopment/service/ProductService.java
@@ -0,0 +1,7 @@
+package com.rozz.api.apidevelopment.service;
+
+import com.rozz.api.apidevelopment.dto.ProductRequest;
+
+public interface ProductService {
+    public void createProduct(ProductRequest productRequest);
+}
