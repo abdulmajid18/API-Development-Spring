13e096914069d75ec6848359189c9f90984ec2a2
abdulmajid18 <nuhuabdulmajid58@gmail.com>
2023-03-06 22:02:54 +0000

    added getAllPproducts to Service

--- api-development/src/main/java/com/rozz/api/apidevelopment/service/ProductService.java
+++ api-development/src/main/java/com/rozz/api/apidevelopment/service/ProductService.java
@@ -1,7 +1,11 @@
 package com.rozz.api.apidevelopment.service;
 
 import com.rozz.api.apidevelopment.dto.ProductRequest;
+import com.rozz.api.apidevelopment.dto.ProductResponse;
+import java.util.List;
 
 public interface ProductService {
     public void createProduct(ProductRequest productRequest);
+
+    public List<ProductResponse> getAllProducts();
 }
