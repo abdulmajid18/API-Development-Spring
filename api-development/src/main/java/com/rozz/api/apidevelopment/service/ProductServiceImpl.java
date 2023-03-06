ed736ce68783431d13a2c72f738325dee610ec77
abdulmajid18 <nuhuabdulmajid58@gmail.com>
2023-03-06 22:03:02 +0000

    added getAllPproducts to Impl

--- api-development/src/main/java/com/rozz/api/apidevelopment/service/ProductServiceImpl.java
+++ api-development/src/main/java/com/rozz/api/apidevelopment/service/ProductServiceImpl.java
@@ -3,8 +3,14 @@ package com.rozz.api.apidevelopment.service;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
+import java.util.List;
+import java.util.Objects;
+import java.util.stream.StreamSupport;
+import java.util.Collections;
+import static java.util.stream.Collectors.toList;
 
 import com.rozz.api.apidevelopment.dto.ProductRequest;
+import com.rozz.api.apidevelopment.dto.ProductResponse;
 import com.rozz.api.apidevelopment.entity.ProductEntity;
 import com.rozz.api.apidevelopment.repository.ProductRepository;
 
@@ -30,7 +36,31 @@ public class ProductServiceImpl implements ProductService {
         repository.save(productEntity);
 
         log.info("Request for Creating Product Name: {}\n", productEntity.getName());
+    }
 
+    public List<ProductResponse> getAllProducts() {
+        Iterable<ProductEntity> productEntities = repository.findAll();
+        return toListModel(productEntities);
+    }
+
+    private ProductResponse mapToProduct(ProductEntity productEntity) {
+        ProductResponse productResponse = new ProductResponse();
+        productResponse.setId(productEntity.getId())
+                .setName(productEntity.getName())
+                .setDescription(productEntity.getDescription())
+                .setCount(productEntity.getCount())
+                .setPrice(productEntity.getPrice())
+                .setImageUrl(productEntity.getImageUrl());
+
+        return productResponse;
+    }
+
+    public List<ProductResponse> toListModel(Iterable<ProductEntity> entities) {
+        if (Objects.isNull(entities)) {
+            return Collections.emptyList();
+        }
+
+        return StreamSupport.stream(entities.spliterator(), false).map(this::mapToProduct).collect(toList());
     }
 
 }
