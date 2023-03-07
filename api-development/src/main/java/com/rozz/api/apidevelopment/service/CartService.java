2ff4f60ee660bfb95208e5a753f874966b2465cd
abdulmajid18 <nuhuabdulmajid58@gmail.com>
2023-03-07 00:00:31 +0000

    change parameter

--- api-development/src/main/java/com/rozz/api/apidevelopment/service/CartService.java
+++ api-development/src/main/java/com/rozz/api/apidevelopment/service/CartService.java
@@ -1,14 +1,11 @@
 package com.rozz.api.apidevelopment.service;
 
-import java.util.Optional;
-
 import com.rozz.api.apidevelopment.entity.CartEntity;
 import com.rozz.api.apidevelopment.entity.ItemEntity;
 
-import java.util.List;
-
 public interface CartService {
-    public Optional<CartEntity> getCartByCustomerId(String id);
+    public CartEntity getCartByCustomerId(String CustomerId);
 
-    public List<ItemEntity> addCartItemsByCustomerId(String customerId, ItemEntity item);
+    // public List<ItemEntity> addCartItemsByCustomerId(String customerId,
+    // ItemEntity item);
 }
