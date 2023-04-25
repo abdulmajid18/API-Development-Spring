334d28948a9b6bf16bd9f644bb1dd091c62eed9c
amnuhu <abdul-majid.nuhu@turntabl.io>
2023-04-25 07:46:54 +0000

    test

---
+++ src/test/java/com/rozz/api/apidevelopment/ApiDevelopmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rozz.api.apidevelopment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiDevelopmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
